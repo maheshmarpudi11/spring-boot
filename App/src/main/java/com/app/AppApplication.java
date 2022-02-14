e39def3979e7650763da39aeab176d90c6347ddf
maheshmarpudi11 <maheshmarpudi11@gmail.com>
2022-02-14 23:20:01 +0530

    14-02-2022

--- App/src/main/java/com/app/AppApplication.java
+++ App/src/main/java/com/app/AppApplication.java
@@ -1,13 +1,21 @@
 package com.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Application Started..!");
+		
+	}
+
+	
 }
